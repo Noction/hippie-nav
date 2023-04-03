317d1fe489d225bb5ac07bd54984b19b3e8e78da
Cristian <sellercsgo@gmail.com>
2023-04-03 23:04:17 +0300

    playground

---
+++ packages/demo/src/components/HippieNavPlayground.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <hippie-nav
+      :routes="routes"
+      :excluded-paths="excludedPaths"
+    />
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+import { RouteRecordNormalized } from 'vue-router'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  computed: {
+    excludedPaths (): Array<string> {
+      return ['/about']
+    },
+    routes (): Array<RouteRecordNormalized> {
+      return this.$router.getRoutes()
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
