650a62264984f6da9d5460a9f63b34b95b00e3b0
Cristian <sellercsgo@gmail.com>
2023-04-06 21:09:46 +0300

    playground, started slot development

--- packages/hippie-nav/src/components/SearchResult.vue
+++ packages/hippie-nav/src/components/SearchResult.vue
@@ -1,12 +1,22 @@
 <template>
-  <h2 class="text">
-    Results
-  </h2>
-  <div v-for="(result, index) in results" :key="result.path">
-    <search-result-item
-      :colored="index === current"
-      :result="result"
-    />
+  <div v-if="results.length > 0">
+    <h2 class="text">
+      Results
+    </h2>
+    <div
+      v-for="(result, index) in results"
+      :key="result.path"
+    >
+      <search-result-item
+        :colored="index === current"
+        :result="result"
+        @mouse-over="$emit('mouse-over', result)"
+        @close-modal="$emit('close-modal')"
+      />
+    </div>
+  </div>
+  <div v-else-if="results.length === 0 && input !== ''">
+    Nothing have found
   </div>
 </template>
 
@@ -23,10 +33,22 @@ export default defineComponent({
       type: Number,
       required: true
     },
+    input: {
+      type: String,
+      required: true
+    },
     results: {
       type: Array as PropType<RouteRecordNormalized[]>,
-      required: true
+      required: false,
+      default: [] as RouteRecordNormalized[]
     }
-  }
+  },
+  emits: ['close-modal', 'mouse-over']
 })
 </script>
+
+<style>
+.color__red {
+  color: rebeccapurple;
+}
+</style>
--- packages/hippie-nav/src/components/SearchResultItem.vue
+++ packages/hippie-nav/src/components/SearchResultItem.vue
@@ -1,7 +1,15 @@
 <template>
-  <h3 class="text" :class="{ selected: colored }">
-    {{ result.name }}
-  </h3>
+  <div
+    :class="{ selected: colored }"
+    @click="goto(result.path)"
+    @mouseover="$emit('mouse-over', result)"
+  >
+    <slot>
+      <h3 class="text search--result__item">
+        {{ result.name }}
+      </h3>
+    </slot>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,21 +17,37 @@ import { PropType, defineComponent } from 'vue'
 import { RouteRecordNormalized } from 'vue-router'
 
 export default defineComponent({
+  name: 'SearchResultItem',
   props: {
-    result: {
-      type: Object as PropType<RouteRecordNormalized>,
-      required: true
-    },
     colored: {
       type: Boolean,
       required: true
+    },
+    result: {
+      type: Object as PropType<RouteRecordNormalized>,
+      required: true
+    }
+  },
+  emits: ['close-modal', 'mouse-over'],
+  methods: {
+    goto (path: string) {
+      this.$router.push(path)
+      this.$emit('close-modal')
     }
   }
 })
 </script>
 
 <style scoped>
-  .selected {
-    background-color: #009175;
-  }
+.selected {
+  background-color: rgba(110, 107, 107, 0.65);
+}
+
+.search--result__item {
+  padding: 7px;
+  border-radius: 10px;
+  color: #1a1a1a;
+  font-weight: 700;
+  cursor: pointer;
+}
 </style>
