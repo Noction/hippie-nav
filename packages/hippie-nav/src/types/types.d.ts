43bdb6b82869ed823ecbd79da628df74dfde0903
Cristian <sellercsgo@gmail.com>
2023-05-02 21:30:19 +0300

    * perf: migrated to @noction/vue-use-flexsearch * chore: added web-types to hippieNav

--- packages/hippie-nav/src/types/types.d.ts
+++ packages/hippie-nav/src/types/types.d.ts
@@ -1,1 +1,5 @@
+import { ActionConfig } from '@/types/interfaces'
+import { RouteRecordNormalized } from 'vue-router'
+
 export type Modifier = { [i: string]: { key: string, pressed: boolean } }
+export type ResultWithId = {id: number} & (ActionConfig | RouteRecordNormalized )
