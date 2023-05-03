695b68af41b8110db3b30bce75ac5f54c5168a0a
Cristian <sellercsgo@gmail.com>
2023-05-03 22:11:19 +0300

    fixed: highlight directive

--- packages/hippie-nav/src/directives/textHighlight.ts
+++ packages/hippie-nav/src/directives/textHighlight.ts
@@ -1,19 +1,24 @@
-import { DirectiveBinding, VNode, onBeforeUnmount, onMounted  } from 'vue'
+import { Directive, VNode } from 'vue'
+/* eslint-disable */
 const replaceWithOriginal = (original: string, newText: string) => `<p style="display:none;">${original}</p>${newText}`
 
-declare global {
-  interface HTMLElement {
-    beforeHighlight: () => void;
-  }
+const escapeHtml = (unsafe: string) => {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
 }
-const escapeHtml = (unsafe: string) => unsafe
-  .replace(/&/g, '&amp;')
-  .replace(/</g, '&lt;')
-  .replace(/>/g, '&gt;')
-  .replace(/"/g, '&quot;')
-  .replace(/'/g, '&#039;')
+
+const unescapeHtml = (safe: string) => safe
+  .replace(/&amp;/g, '&')
+  .replace(/&lt;/g, '<')
+  .replace(/&gt;/g, '>')
+  .replace(/&quot;/g, '"')
+  .replace(/&#039;/g, '\'')
+
 const escapeRegExp = function (str: string) {
-  // eslint-disable-next-line
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|\<\>]/g, '\\$&')
 }
 
@@ -22,11 +27,17 @@ const highlightSearch = function (message: string, keyword: string) {
   const newKeyword = keyword
   let regexWord = ''
 
-  if (/^\s*$/.test(keyword)) {
-    // when the keyword is empty string, return the original message.
+  if (typeof keyword === 'string') {
+    if (/^\s*$/.test(keyword)) {
+      // when the keyword is empty string, return the original message.
+      return escapeHtml(message)
+    }
+    regexWord = escapeRegExp(newKeyword)
+  } else {
+    console.warn('type is not String')
+    // return ''
     return escapeHtml(message)
   }
-  regexWord = escapeRegExp(newKeyword)
 
   // const match = new RegExp(`(${regexWord})`, flags)
   // Can only replace the words out of the html tags.
@@ -35,53 +46,55 @@ const highlightSearch = function (message: string, keyword: string) {
 
   if (testMath) {
     // return escapeHtml(message).replace(match, `<span ${styleString}>\$&</span>`)
-    // eslint-disable-next-line
     const replaced = message.replace(match, ':;{{:;\$&:;}}:;')
     const matchAgain = new RegExp(`:;{{:;(${escapeHtml(regexWord)}):;}}:;`, 'gi')
-    // eslint-disable-next-line
-    return escapeHtml(replaced).replace(matchAgain, `<span class="hippie-highlighted" ${styleString}>\$1</span>`)
+
+    const restoreReplaced = escapeHtml(replaced).replace(matchAgain, `<span class="highlighted" ${styleString}>\$1</span>`)
+
+    return restoreReplaced
   }
   return escapeHtml(message)
 }
 
-export default {
-  mounted (el: HTMLElement, binding: DirectiveBinding) {
+const beforeHighlight = (el: Element, binding: { value: { keyword: string }}, original: string) => {
+  const {
+    value: { keyword }
+  } = binding
+
+  if (!keyword || keyword === '') {
+    const escaseOriginal = escapeHtml(original)
+
+    el.innerHTML = replaceWithOriginal(escaseOriginal, escaseOriginal)
+    return
+  }
+
+  const highlight = highlightSearch(original, keyword)
+
+  el.innerHTML = replaceWithOriginal(escapeHtml(original), highlight)
+}
+
+interface HighlightSearchDirective {
+  mounted: (el: Element, binding: { value: { keyword: string } }) => void
+  updated: (el: Element, binding: { value: { keyword: string } }, vnode: VNode) => void
+  unmounted: (el: Element) => void
+}
+const textHighlight: HighlightSearchDirective & Directive =  {
+  mounted (el: Element, binding: { value: { keyword: string }}) {
     const originalString = `${el.innerHTML}`
 
     el.innerHTML = replaceWithOriginal(originalString, originalString)
-    const beforeHighlight = () => {
-      const {
-        value: { keyword }
-      } = binding
-
-      if (!keyword || keyword === '') {
-        const escaseOriginal = escapeHtml(originalString)
-
-        el.innerHTML = replaceWithOriginal(escaseOriginal, escaseOriginal)
-        return
-      }
-
-      const highlight = highlightSearch(originalString, keyword)
-
-      el.innerHTML = replaceWithOriginal(escapeHtml(originalString), highlight)
-    }
-
-    beforeHighlight()
-
-    onMounted(() => {
-      el.beforeHighlight = beforeHighlight
-    })
-
-    onBeforeUnmount(() => {
-      el.innerHTML = el.children[0].innerHTML
-    })
+    beforeHighlight(el, binding, unescapeHtml(originalString))
   },
-  updated (el: HTMLElement, binding: DirectiveBinding, vnode: VNode) {
-    if (vnode.children !== null) {
-      const originalString = escapeHtml(vnode.children[0].text)
+  unmounted (el: Element) {
+    el.innerHTML = el.children[0].innerHTML
+  },
+  updated (el: Element, binding: { value: { keyword: string }}, vnode: VNode) {
+    const originalString = vnode.children[0].el.textContent
 
-      el.innerHTML = replaceWithOriginal(originalString, originalString)
-      el.beforeHighlight()
-    }
+    el.innerHTML = replaceWithOriginal(originalString, originalString)
+    beforeHighlight(el, binding, unescapeHtml(originalString))
   }
+
 }
+
+export default textHighlight
