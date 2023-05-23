9973af1d48c7e9e3d8bfe871fa0f52c1d031c817
Cristian Gratii <cgratii@noction.com>
2023-05-23 18:58:53 +0300

    * test: added test for HippieNav Component * refactor: HippieNav, SearchPan * chore: added xmlns to svgs

--- packages/hippie-nav/tests/unit/HippieNav.spec.ts
+++ packages/hippie-nav/tests/unit/HippieNav.spec.ts
@@ -1,38 +1,97 @@
 import { HippieNav } from '@/index'
-import { describe, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
-import { defineComponent } from 'vue'
+import SearchPan from '@/components/SearchPan.vue'
+import simulateShortcut from '../utils/simulateShorcut'
+import { VueWrapper, enableAutoUnmount, mount } from '@vue/test-utils'
+import { afterEach, beforeEach, describe, expect } from 'vitest'
 import { createRouter, createWebHistory } from 'vue-router'
+import { defineComponent, nextTick } from 'vue'
 
 const AppComponent = defineComponent({
   name: 'AppComponent',
   components: { HippieNav },
-  template: '<template><hippie-nav/></template>'
+  template: '<div><hippie-nav/></div>'
 })
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [{
     component: AppComponent,
-    name: 'Hippie',
+    name: 'Main page',
     path: '/'
+  }, {
+    component: AppComponent,
+    name: 'Home Page',
+    path: '/home'
+  }, {
+    component: AppComponent,
+    name: 'About page',
+    path: '/about'
   }]
 })
 
 describe('HippieNav component', () => {
-  it('should open on meta+K', async function () {
+  let wrapper: VueWrapper<InstanceType<typeof AppComponent>>
 
-    const wrapper = mount(AppComponent, {
+  beforeEach(() => {
+    wrapper = mount(AppComponent, {
       global: {
         plugins: [router]
       }
     })
-
-    const hippieNav = wrapper.findComponent(HippieNav)
-
-    await hippieNav.trigger('keydown', { key: 'meta' })
-    await hippieNav.trigger('keydown', { key: 'k' })
-
-    expect(hippieNav.vm.showModal).toBeTruthy()
   })
+
+  enableAutoUnmount(afterEach)
+
+  describe('shortcut', function () {
+    it('should open on meta+K', async function () {
+      const hippieNav = wrapper.findComponent(HippieNav)
+
+      simulateShortcut('Meta', 'k')
+
+      expect(hippieNav.vm.showModal).toBe(true)
+    })
+
+    it('should open on meta+K', async function () {
+      const hippieNav = wrapper.findComponent(HippieNav)
+
+      simulateShortcut('Control', 'k')
+
+      expect(hippieNav.vm.showModal).toBe(true)
+    })
+
+    it('should not open on meta+P', async function () {
+      const hippieNav = wrapper.findComponent(HippieNav)
+
+      simulateShortcut('Meta', 'P')
+
+      expect(hippieNav.vm.showModal).toBe(false)
+    })
+  })
+
+  describe('searchPan', () => {
+    it('should have one item in results', async function () {
+      simulateShortcut('Meta', 'k')
+      await nextTick()
+      const hippieNav = wrapper.getComponent(HippieNav)
+      const searchPan = wrapper.getComponent(SearchPan)
+      const input = searchPan.get({ ref: 'input' })
+
+      await input.setValue('Home')
+
+      expect(hippieNav.vm.results).toHaveLength(1)
+    })
+
+    it('should have three items in results', async function () {
+      simulateShortcut('Meta', 'k')
+      await nextTick()
+      const hippieNav = wrapper.getComponent(HippieNav)
+      const searchPan = wrapper.getComponent(SearchPan)
+      const input = searchPan.get({ ref: 'input' })
+
+      await input.setValue('Page')
+
+      expect(hippieNav.vm.results).toHaveLength(3)
+    })
+  })
+
 })
