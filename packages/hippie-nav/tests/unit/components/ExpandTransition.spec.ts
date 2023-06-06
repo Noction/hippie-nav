384f745170e85527baddc9cec9484780613f3ed7
Cristian Gratii <cgratii@noction.com>
2023-06-06 14:08:55 +0300

    * tests: added a new cases for HippieNav, ExpandTransition * chore: added nav buttons to recentResults

---
+++ packages/hippie-nav/tests/unit/components/ExpandTransition.spec.ts
@@ -0,0 +1,37 @@
+import ExpandTransition from '@/components/ExpandTransition.vue'
+import { defineComponent } from 'vue'
+import { mount } from '@vue/test-utils'
+
+const component = defineComponent({
+  name: 'HelloWorld',
+  components: {
+    ExpandTransition
+  },
+  template: '<div><ExpandTransition><svg/></ExpandTransition></div>'
+})
+
+describe('ExpandTransition', () => {
+  it('should throw a error if given Element is not HTMLElement on enter', function () {
+    const wrapper = mount(component)
+
+    const element= wrapper.findComponent(ExpandTransition)
+
+    expect(() => { (element.vm as any).enter() }).toThrowError(new Error('Element should be HTMLElement'))
+  })
+
+  it('should throw a error if given Element is not HTMLElement on leave', function () {
+    const wrapper = mount(component)
+
+    const element= wrapper.findComponent(ExpandTransition)
+
+    expect(() => { (element.vm as any).leave() }).toThrowError(new Error('Element should be HTMLElement'))
+  })
+
+  it('should throw a error if given Element is not HTMLElement on afterEnter', function () {
+    const wrapper = mount(component)
+
+    const element= wrapper.findComponent(ExpandTransition)
+
+    expect(() => { (element.vm as any).leave() }).toThrowError(new Error('Element should be HTMLElement'))
+  })
+})
